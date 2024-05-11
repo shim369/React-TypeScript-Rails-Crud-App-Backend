02c794329ef4424ad01b181706addfafaf73e293
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-11 17:38:47 +0900

    update controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,31 @@
 class PostsController < ApplicationController
 	def index
-		render json: Post.all, methods: [:image_url]
+	  render json: Post.all, methods: [:image_url]
+	end
+
+	def show
+	  post = Post.find(params[:id])
+	  render json: post, methods: [:image_url]
 	end
 
 	def create
-		post = Post.new(post_params)
-		if post.save
-			render json: post, methods: [:image_url]
-		else
-			render json: post.errors, status: 422
-		end
+	  post = Post.new(post_params)
+	  if post.save
+		render json: post, methods: [:image_url], status: :created
+	  else
+		render json: post.errors, status: :unprocessable_entity
+	  end
 	end
 
 	def destroy
-		post = Post.find(params[:id])
-		post.destroy!
-		render json: post
+	  post = Post.find(params[:id])
+	  post.destroy!
+	  render json: post
 	end
 
 	private
 
 	def post_params
-		params.permit(:title, :image)
+	  params.permit(:title, :image)
 	end
 end
