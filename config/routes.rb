527993568d04f8ea9e7510e6926a0f0c70e5aa21
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-11 17:39:02 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   scope :api do
-    resources :posts, only: [:index, :create, :destroy]
+    resources :posts, only: [:index, :create, :show, :destroy]
   end
 end
