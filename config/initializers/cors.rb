a27cb55344c986e07ac6ed00634f6e4b06d72b1c
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-11 17:39:41 +0900

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://127.0.0.1:3000"
-
-    resource "*",
+    origins "http://localhost:3000"
+    resource '/api/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
