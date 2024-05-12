b551050bdcd6d1687914a8770a0dc1345f29a546
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-12 12:35:50 +0900

    update host & post

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  Rails.application.routes.default_url_options[:host] = 'localhost'
-  Rails.application.routes.default_url_options[:port] = 3000
+  Rails.application.routes.default_url_options[:host] = '127.0.0.1'
+  Rails.application.routes.default_url_options[:port] = 5000
 end
